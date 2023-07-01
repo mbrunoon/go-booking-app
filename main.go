04d2be83a46173dd4533be4092e1222fd1109de9
mbrunoon <contato@marcosbruno.com>
2023-07-01 23:47:16 +0100

    feat: "Encapsulate logic with Functions" section

--- main.go
+++ main.go
@@ -5,72 +5,35 @@ import (
 	"strings"
 )
 
+// Package Level Variables
+// They can be accessed inside any of the functions and all files which are in the same package
+
+// variable declarations with datatype determined by the value assigned
+const conferenceTickets = 50
+
+var remainingTickets uint = 50
+var conferenceName = "Coffee Conference"
+
+// conferenceName := "Coffee Conference" // a shorthand for variable declaration with a datapyte but its not allowed in the Package Level Variables
+
+// var bookings [50]string // array with 50 positions and no initial value
+var bookings []string // slice: dynamic array (no lenght defined)
+
 func main() {
 
-	// variable declarations with datatype determined by the value assigned
-	const conferenceTickets = 50
-	var remainingTickets uint = 50
-	conferenceName := "Coffee Conference" // a shorthand for variable declaration with a datapyte
-
-	// var bookings [50]string // array with 50 positions and no initial value
-	var bookings []string // slice: dynamic array (no lenght defined)
-
-	// printing strings using placeholders
-	fmt.Printf("Welcom to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %v ticket and %v are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
+	greetUsers()
 
 	for remainingTickets > 0 && len(bookings) < 50 {
 
-		// declaring variables without specify the values (you have to define the type)
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint // uint is a datatype that only allows positive numbers
+		firstName, lastName, email, userTickets := getUserInput()
 
-		// alternative syntax to create a slice:
-		//   var bookings = []string{}
-		// or
-		//   bookings := []string{}
-
-		// Asking user for inputs
-
-		fmt.Println("Enter your firstname:")
-		fmt.Scan(&firstName)
-
-		fmt.Println("Enter your lastname:")
-		fmt.Scan(&lastName)
-
-		fmt.Println("Enter your email:")
-		fmt.Scan(&email)
-
-		fmt.Println("Enter number of tickets:")
-		fmt.Scan(&userTickets)
-
-		// & represents a **pointer** (a variable that points to the memory address of another variable that references the actual value). Pointers in Go are also called special variables
-
-		// If you use the scan without the pointer the method dont wait for the user input because its working with a copy from the variable and can´t change the original value
-
-		isValidName := len(firstName) >= 2 && len(lastName) >= 2
-		isValidEmail := strings.Contains(email, "@")
-		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		isValidName, isValidEmail, isValidTicketNumber := validateUserInput(firstName, lastName, email, userTickets)
 
 		if isValidName && isValidEmail && isValidTicketNumber {
 
-			remainingTickets = remainingTickets - userTickets
-			bookings = append(bookings, firstName+" "+lastName)
+			bookTicket(userTickets, firstName, lastName, email)
 
-			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-
-			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-
-			firstNames := []string{}
-			for _, booking := range bookings { // user _ to especify unused variables or those ones you don't want to use
-				var names = strings.Fields(booking) // splits the string with white space as separator and returns a slice with the slip elements
-				firstNames = append(firstNames, names[0])
-			}
-			// range expression allow us iterate over the elements and data structures
-
+			firstNames := getFirstNames()
 			fmt.Printf("The firsts names of bookings are: %v\n", firstNames)
 
 			if remainingTickets == 0 {
@@ -94,4 +57,74 @@ func main() {
 
 	}
 
+}
+
+func greetUsers() {
+	// printing strings using placeholders
+	fmt.Printf("Welcom to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v ticket and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+}
+
+func getFirstNames() []string {
+	firstNames := []string{}
+	for _, booking := range bookings { // user _ to especify unused variables or those ones you don't want to use
+		var names = strings.Fields(booking) // splits the string with white space as separator and returns a slice with the slip elements
+		firstNames = append(firstNames, names[0])
+	}
+	// range expression allow us iterate over the elements and data structures
+
+	return firstNames
+}
+
+// function with multiples returns
+func validateUserInput(firstName string, lastName string, email string, userTickets uint) (bool, bool, bool) {
+	isValidName := len(firstName) >= 2 && len(lastName) >= 2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+	// returning multiples values
+	return isValidName, isValidEmail, isValidTicketNumber
+}
+
+func getUserInput() (string, string, string, uint) {
+	// declaring variables without specify the values (you have to define the type)
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint // uint is a datatype that only allows positive numbers
+
+	// alternative syntax to create a slice:
+	//   var bookings = []string{}
+	// or
+	//   bookings := []string{}
+
+	// Asking user for inputs
+
+	fmt.Println("Enter your firstname:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your lastname:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email:")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets:")
+	fmt.Scan(&userTickets)
+
+	// & represents a **pointer** (a variable that points to the memory address of another variable that references the actual value). Pointers in Go are also called special variables
+
+	// If you use the scan without the pointer the method dont wait for the user input because its working with a copy from the variable and can´t change the original value
+
+	return firstName, lastName, email, userTickets
+}
+
+func bookTicket(userTickets uint, firstName string, lastName string, email string) {
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
